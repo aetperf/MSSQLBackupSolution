4b2930f48880771deee80290393fcdb7b8b03aef
rferraton <16419423+rferraton@users.noreply.github.com>
2025-03-10 21:12:11 +0100

    Manage error and logging problems in the Purge scripts

--- PurgeFiles.ps1
+++ PurgeFiles.ps1
@@ -42,141 +42,146 @@
 
     #>
 
-param 
-(
-    [Parameter(Mandatory)] [ValidateSet('Full','Diff','Log','ShellLog')] [string] $FileType,
-    [Parameter()] [string] $RootDirectory,
-    [Parameter()] [string] $LogDirectory,
-    [Parameter()] [int] $HoursDelay=168, #24*7
-    [Parameter()] [string] $LogLevel = "INFO"
-)
-
-
-#############################################################################################
-## LOGGING PREPARATION
-#############################################################################################
-
-Set-LoggingDefaultLevel -Level INFO
-$ProgressPreference = "SilentlyContinue"
-Add-LoggingTarget -Name Console -Configuration @{
-    ColorMapping = @{
-        DEBUG = 'Gray'
-        INFO  = 'White'
-        ERROR  = 'DarkRed'
-    };
-    Level='DEBUG'
-}
-
-if ($PSBoundParameters.ContainsKey('LogDirectory'))
-{   
-    if ($LogDirectory -ne "")
-    {
-        try{
-            $TimestampLog=Get-Date -UFormat "%Y-%m-%d"
-            if (-not(Test-Path $LogDirectory)) {mkdir $LogDirectory -Force}
-            $LogfileName="Purge_${FileType}_${TimestampLog}.log"
-            $LogFile= Join-DbaPath -Path $LogDirectory -Child $LogfileName
-            Add-LoggingTarget -Name File -Configuration @{Level = 'INFO'; Path = $LogFile}
-            Write-Log -Level INFO -Message "Log File : $LogFile"
-        }
-        catch{
-            Write-Log -Level ERROR -Message "Failed to initialize log file in ${LogDirectory}"
-            Exit 1
-        }
-    }
-}
-
-
-$PurgeStartTimeStamp=Get-Date
-
-Write-Log -Level INFO -Message "Parameter FileType : ${FileType}"
-Write-Log -Level INFO -Message "Parameter RootDirectory : ${RootDirectory}"
-Write-Log -Level INFO -Message "Parameter HoursDelay : ${HoursDelay}"
-
-
-#############################################################################################
-## PURGE PREPARATION : Get the extension file
-#############################################################################################
-
-
+    param 
+    (
+        [Parameter(Mandatory)] [ValidateSet('Full','Diff','Log','ShellLog')] [string] $FileType,
+        [Parameter()] [string] $RootDirectory,
+        [Parameter()] [string] $LogDirectory,
+        [Parameter()] [int] $HoursDelay=168, #24*7
+        [Parameter()] [string] $LogLevel = "INFO"
+    )
     
-switch ( $FileType ) 
-    {
-        "Full" 
-            {
-                $FileExtension="bak"  
-            }
-        "Diff" 
-            {
-                $FileExtension="diff" 
-            }
-        "Log"  
-            {
-                $FileExtension="trn"  
-            } 
-        "ShellLog"  
-            {
-                $FileExtension="log"  
-            }    
+    
+    #############################################################################################
+    ## LOGGING PREPARATION
+    #############################################################################################
+    Set-LoggingDefaultLevel -Level INFO
+    $ProgressPreference = "SilentlyContinue"
+    Add-LoggingTarget -Name Console -Configuration @{
+        ColorMapping = @{
+            DEBUG = 'Gray'
+            INFO  = 'White'
+            ERROR  = 'DarkRed'
+        };
+        Level='DEBUG'
     }
-
-Write-Log -Level INFO -Message "File Extension : .${FileExtension}"
-
-#############################################################################################
-## PURGE
-#############################################################################################
-
-$ExitCode=0
-
-if (-not(Test-Path $RootDirectory)) {
-       Write-Log -Level ERROR -Message "Failed ${RootDirectory} directory does not exist" 
-    Exit 1   
-}
-
-try {
-    $TimeDelay = (Get-Date).AddHours(-$HoursDelay)
-    $result=Get-ChildItem -Path $RootDirectory -Recurse -Include *.$FileExtension -Exclude $LogfileName | Where-Object { $_.LastWriteTime -lt $TimeDelay } 
-    $NbFiles=$result.Count
-    Write-Log -Level INFO -Message "Found ${NbFiles} files to delete"
-}
-catch
-{
-    Write-Log -Level ERROR -Message "Problem when trying to list ${FileExtension} files on ${RootDirectory}" 
-    $ExitCode=1
-}
-
-if ($ExitCode -eq 0)
-{
+    
+    $CurrentLogFileConfig=Get-LoggingTarget -Name File
+    
+    if ($PSBoundParameters.ContainsKey('LogDirectory'))
+    {   
+        if ($LogDirectory -ne "")
+        {
+            try{
+                $TimestampLog=Get-Date -UFormat "%Y-%m-%d"
+                if (-not(Test-Path $LogDirectory)) {mkdir $LogDirectory -Force}
+                $LogfileName="Purge_${FileType}_${TimestampLog}.log"
+                $LogFile= Join-DbaPath -Path $LogDirectory -Child $LogfileName
+                if ($CurrentLogFileConfig.Path -ne $LogFile)
+                {
+                    Add-LoggingTarget -Name File -Configuration @{Level = 'INFO'; Path = $LogFile}
+                }
+                Write-Log -Level INFO -Message "Log File : $LogFile"
+            }
+            catch{
+                Write-Log -Level ERROR -Message "Failed to initialize log file in ${LogDirectory}"
+                Exit 1
+            }
+        }
+    }
+    
+    
+    $PurgeStartTimeStamp=Get-Date
+    
+    Write-Log -Level INFO -Message "Parameter FileType : ${FileType}"
+    Write-Log -Level INFO -Message "Parameter RootDirectory : ${RootDirectory}"
+    Write-Log -Level INFO -Message "Parameter HoursDelay : ${HoursDelay}"
+    
+    
+    #############################################################################################
+    ## PURGE PREPARATION : Get the extension file
+    #############################################################################################
+    
+    
+        
+    switch ( $FileType ) 
+        {
+            "Full" 
+                {
+                    $FileExtension="bak"  
+                }
+            "Diff" 
+                {
+                    $FileExtension="diff" 
+                }
+            "Log"  
+                {
+                    $FileExtension="trn"  
+                } 
+            "ShellLog"  
+                {
+                    $FileExtension="log"  
+                }    
+        }
+    
+    Write-Log -Level INFO -Message "File Extension : .${FileExtension}"
+    
+    #############################################################################################
+    ## PURGE
+    #############################################################################################
+    
+    $ExitCode=0
+    
+    if (-not(Test-Path $RootDirectory)) {
+           Write-Log -Level ERROR -Message "Failed ${RootDirectory} directory does not exist" 
+        Exit 1   
+    }
+    
     try {
-        $result | ForEach-Object {
-            $fileName=$_.FullName
-            Write-Log -Level INFO -Message "Starting the delete of ${fileName}"
-            Remove-Item -Path $_.FullName
-        }        
+        $TimeDelay = (Get-Date).AddHours(-$HoursDelay)
+        $result=Get-ChildItem -Path $RootDirectory -Recurse -Include *.$FileExtension -Exclude $LogfileName | Where-Object { $_.LastWriteTime -lt $TimeDelay } 
+        $NbFiles=$result.Count
+        Write-Log -Level INFO -Message "Found ${NbFiles} files to delete"
     }
-    catch {    
-        Write-Log -Level ERROR -Message "Problem when trying to remove ${fileName}"
+    catch
+    {
+        Write-Log -Level ERROR -Message "Problem when trying to list ${FileExtension} files on ${RootDirectory}" 
         $ExitCode=1
     }
-}
-
-
-
-#############################################################################################
-## Finish wih Compute ExitCode : Exit with non zero if purge failed
-#############################################################################################
-
-$PurgeEndTimeStamp=Get-Date
-$tspan= New-TimeSpan -Start $PurgeStartTimeStamp -End $PurgeEndTimeStamp
-$PurgeDuration=$tspan.TotalSeconds
-
-if($ExitCode -eq 0){
-    Write-Log -Level INFO -Message "Purge of .${FileExtension} files in ${RootDirectory} : Successful purge of ${NbFiles} files in ${PurgeDuration} seconds"
-}
-else{
-    Write-Log -Level ERROR -Message "Purge of .${FileExtension} files in ${RootDirectory} : Failed in ${PurgeDuration} seconds"
-}
-
-Wait-Logging
-
-Exit $ExitCode
+    
+    if ($ExitCode -eq 0)
+    {
+        try {
+            $result | ForEach-Object {
+                $fileName=$_.FullName
+                Write-Log -Level INFO -Message "Starting the delete of ${fileName}"
+                Remove-Item -Path $fileName -force
+            }        
+        }
+        catch {    
+            Write-Log -Level ERROR -Message "Problem when trying to remove ${fileName}"
+            $ExitCode=1
+        }
+    }
+    
+    
+    
+    #############################################################################################
+    ## Finish wih Compute ExitCode : Exit with non zero if purge failed
+    #############################################################################################
+    
+    $PurgeEndTimeStamp=Get-Date
+    $tspan= New-TimeSpan -Start $PurgeStartTimeStamp -End $PurgeEndTimeStamp
+    $PurgeDuration=$tspan.TotalSeconds
+    
+    if($ExitCode -eq 0){
+        Write-Log -Level INFO -Message "Purge of .${FileExtension} files in ${RootDirectory} : Successful purge of ${NbFiles} files in ${PurgeDuration} seconds"
+    }
+    else{
+        Write-Log -Level ERROR -Message "Purge of .${FileExtension} files in ${RootDirectory} : Failed in ${PurgeDuration} seconds"
+    }
+    
+    Wait-Logging
+    
+    Exit $ExitCode
+    
